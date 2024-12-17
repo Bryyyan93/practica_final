9ed540319f8458c64074afa8de402f6454751e61
soniamarlo <63555832+soniamarlo@users.noreply.github.com>
2024-12-17 19:12:03 +0100

    fix:"Update gke-cluster.tf"

--- gcp/gke-cluster.tf
+++ gcp/gke-cluster.tf
@@ -3,16 +3,16 @@ resource "google_container_cluster" "primary" {
   name     = "gke-cluster"
   location = "us-central1"
 
-  initial_node_count = 1
+  initial_node_count = 2
 
   # Desactivar protección de eliminación
   deletion_protection = false
 
   node_config {
     # Máquina más pequeña y económica.
-    machine_type = "e2-small"
+    machine_type = "e2-medium"
     # Reducir el tamaño del disco a 50 GB en lugar de 100 GB por nodo.
-    disk_size_gb = 10
+    disk_size_gb = 20
     disk_type    = "pd-standard" # Usar discos estándar en lugar de SSD.
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
